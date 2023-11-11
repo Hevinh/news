105a62ca9e8f5ec4ae3c217e1b7bc7bbb62980fc
Hevinh <147808228+Hevinh@users.noreply.github.com>
2023-11-11 13:15:06 +0530

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function validateLogin() {
     var password = document.getElementById('password').value;
 
     if (username === 'admin@123' && password === 'admin123') {
-        window.location.href=contact.html
+        
         return true; // Allow the form submission
     } else {
         alert('Invalid username or password');
